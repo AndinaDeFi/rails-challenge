5dd519a75be426b024273c8a4f6f0dbf966882a1
Santiago Llapur <santillapur@gmail.com>
2021-07-15 13:07:47 -0300

    feat[cryptocurrencies]: Add uniqueness validation in model

--- app/models/cryptocurrency.rb
+++ app/models/cryptocurrency.rb
@@ -2,4 +2,5 @@ class Cryptocurrency < ApplicationRecord
   enum pair: { USD: 0 }
   validates :name, :symbol, :pair, :price, :market_cap, :total_volume, presence: true
   validates :price, :market_cap, :total_volume, numericality: true
+  validates :name, :symbol, uniqueness: true
 end
