81c358ed56e7a9059f9976032aef5b6357faa99b
Santiago Llapur <santillapur@gmail.com>
2021-07-16 13:43:37 -0300

    feat[favorite]: Add model associations

--- app/models/cryptocurrency.rb
+++ app/models/cryptocurrency.rb
@@ -1,5 +1,12 @@
 class Cryptocurrency < ApplicationRecord
+  # Associations
+  has_many :favorite_cryptocurrencies, dependent: :destroy
+  has_many :users, through: :favorite_cryptocurrencies, source: :user
+
+  # Enum
   enum pair: { USD: 0 }
+
+  # Validations
   validates :name, :symbol, :pair, :price, :market_cap, :total_volume, presence: true
   validates :price, :market_cap, :total_volume, numericality: true
   validates :name, :symbol, uniqueness: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, # :registerable,
          :recoverable, :rememberable, :validatable
 
+  # Associations
+  has_many :favorite_cryptocurrencies, dependent: :destroy
+  has_many :cryptocurrencies, through: :favorite_cryptocurrencies, source: :cryptocurrency
+
+  # Validations
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX, message: 'only email format' }
   validates :password, presence: true, length: { minimum: 6 }
 end
